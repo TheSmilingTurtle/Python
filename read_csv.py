02629e0d5c867feea4ca4f2f93fe7b3fc2aa911b
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-03-24 10:37:02 +0100

    adjusted graph size

--- read_csv.py
+++ read_csv.py
@@ -8,5 +8,5 @@ with open(r"C:\Users\vojta\Documents\Coding\Python\ToGraph.csv", "r") as file:
     for row in reader:
         l.append((float(row[0]), float(row[1])))
 
-plt.scatter(*zip(*l))
+plt.scatter(*zip(*l), s=1)
 plt.show()
