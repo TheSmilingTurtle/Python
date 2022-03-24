5bf12c00c76735cfec44de448dc18d01b5056ee6
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-03-24 10:25:20 +0100

    Initial Commit

--- read_csv.py
+++ read_csv.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 l = []
 
-with open("ToGraph.csv") as file:
+with open(r"C:\Users\vojta\Documents\Coding\Python\ToGraph.csv", "r") as file:
     reader = csv.reader(file)
     for row in reader:
-        l.append((float(row.strip().split(",")[0]), float(row.strip().split(",")[1])))
+        l.append((float(row[0]), float(row[1])))
 
-plt.scatter(zip(*l))
+plt.scatter(*zip(*l))
 plt.show()
