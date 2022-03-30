490e8e8d2fcd86720bd8043251c2810c689fb878
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-03-30 14:18:11 +0200

    csv improvements

--- test/test.py
+++ test/test.py
@@ -2,12 +2,14 @@ import csv
 import math
 import random
 
-l = [[math.sin(i*math.pi/50), math.cos(i*math.pi/50), 1-2*random.random()] for i in range(100)]
+length = 200
+
+l = [["", math.sin(i*math.pi/50), math.cos(i*math.pi/50), 1-2*random.random()] for i in range(length)]
 
 with open("test/Test.csv", "w+", newline="") as file:
     writer = csv.writer(file, delimiter=";")
 
-    for i in range(100):
+    for i in range(length):
         writer.writerow(l[i])
 
 file.close()
