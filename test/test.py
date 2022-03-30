106ba396b31def72dcda0986e5020a8612f89a78
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-03-30 15:38:46 +0200

    csv

--- test/test.py
+++ test/test.py
@@ -1,15 +1,19 @@
 import csv
 import math
 import random
+import time
 
 length = 100
+init_time = time.time()
 
-l = [["", -math.cos(i*math.pi/25), math.sin(i*math.pi/25), 1-2*random.random()] for i in range(length)]
+l = [[-math.cos(i*math.pi/25), -math.sin(i*math.pi/25), 1-2*random.random()*random.random()] for i in range(length)]
 
 with open("test/Test.csv", "w+", newline="") as file:
     writer = csv.writer(file, delimiter=";")
 
     for i in range(length):
-        writer.writerow(l[i])
+        OUTLIST = ["", time.time()-init_time]+l[i]
+        writer.writerow(OUTLIST)
+        time.sleep(0.01)
 
 file.close()
