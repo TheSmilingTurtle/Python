adec0f6d7b7702f73b2ac8213ebecd5ba63a85df
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-04-05 18:51:33 +0200

    nothing seems to work, maybe bezier != PatchPath

--- test/bezier.py
+++ test/bezier.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import matplotlib.path as mpath
 import matplotlib.patches as mpatches
 
@@ -7,6 +8,7 @@ class bezier:
         self.fill_colour = fill_colour
         self.points = args
         self.mat_path = mpath.Path
+        self.types = [self.mat_path.MOVETO]
         self.path = None
 
     def build_path(self) -> None:
@@ -14,19 +16,24 @@ class bezier:
         
         self.path = mpatches.PathPatch(self.mat_path(
             self.points,
-            self.get_types),
+            self.types),
             fc = self.fill_colour
         )
+
+    def add(self, bezier):
+        self.points += bezier.points
+        self.types += bezier.types
     
     @property
     def get_path(self) -> mpath.Path:
         return self.path
     
-    @property
     def get_types(self):
         match self.type:
             case "c":
                 self.types = [self.mat_path.CURVE3 for _ in self.points]
                 self.types[0] = self.mat_path.MOVETO
-
-        return self.types
+            case "l":
+                self.types = [self.mat_path.LINETO]
+            case "h":
+                self.types = [self.mat_path.LINETO]
--- test/test.py
+++ test/test.py
@@ -1,14 +1,28 @@
 import matplotlib.pyplot as plt
 import bezier
 
-curve = bezier.bezier("c", [(0, 0), (0, 1), (1, 1)])
+ax = plt.subplot()
+curve = bezier.bezier("", [(0,0)])
+curve.get_types()
 
-print(curve.points)
+with open("test\\nic_coords.txt") as file:
+    for line in file:
+        uncleaned = line.strip().split(" ")
+        if uncleaned[-1] == ",":
+            uncleaned.pop()
+        cleaned = [float(x.replace(",", "").strip()) for x in uncleaned[1:]]
+        paired = [(x, y) for x, y in zip(cleaned, cleaned[1::2])]
+        temp = bezier.bezier(uncleaned[0].strip(), paired)
+        temp.get_types()
+        curve.add(temp)
+
+print(curve.types)
 
 curve.build_path()
 
-ax = plt.subplot()
-
 ax.add_patch(curve.get_path)
+
+plt.xlim(-300, 600)
+plt.ylim(-300, 600)
 
 plt.show()
